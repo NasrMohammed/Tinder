db98f2bdc2288ab808a7952b86e2cff0e3b9a639
NasrSyncBac <oshar2008@gmail.com>
2019-06-19 16:48:34 -0500

    Added login controller

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -23,8 +23,40 @@ class ViewController: UIViewController {
     }
 
     @objc func wasDragged(gesturerecognizer: UIPanGestureRecognizer) {
-        print("dragged")
+       let labelPoint = gesturerecognizer.translation(in: view)
+        swipeLabel.center = CGPoint(x: view.bounds.width/2 + labelPoint.x, y: view.bounds.height/2 + labelPoint.y)
+        
+        let xFromCenter = view.bounds.width / 2 - swipeLabel.center.x
 
+        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
+        
+        let scale =  min(100 / abs(xFromCenter), 1)
+        
+        var scaledAndRotated = rotation.scaledBy(x: scale, y: scale)
+        
+        swipeLabel.transform = scaledAndRotated
+        
+        
+        // print(swipeLabel.center.x)
+        if gesturerecognizer.state == .ended {
+            if swipeLabel.center.x < (view.bounds.width / 2 - 100) {
+                print("Not Interested")
+            }
+            if swipeLabel.center.x > (view.bounds.width / 2 + 100) {
+                print("Interested")
+            }
+            
+            rotation = CGAffineTransform(rotationAngle: 0)
+            
+            scaledAndRotated = rotation.scaledBy(x: 1, y: 1)
+            
+            swipeLabel.transform = scaledAndRotated
+
+            
+            swipeLabel.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
+            
+            
+        }
     }
 
 }
