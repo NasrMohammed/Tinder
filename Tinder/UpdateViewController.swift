732eb1c1fb7eb00f7cce458c8fe613d6cd521522
NasrSyncBac <oshar2008@gmail.com>
2019-06-20 17:01:00 -0500

    Added Update view controller

---
+++ Tinder/UpdateViewController.swift
@@ -0,0 +1,59 @@
+//
+//  UpdateViewController.swift
+//  Tinder
+//
+//  Created by Nasr Mohammed on 6/20/19.
+//  Copyright © 2019 Nasr Mohammed. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class UpdateViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userGenderSwitch: UISwitch!
+    
+    @IBOutlet weak var interestedGenderSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func updateImageTapped(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func updateTapped(_ sender: Any) {
+        PFUser.current()?["isFemale"] = userGenderSwitch.isOn
+        PFUser.current()?["isInterestedInWomen"] = interestedGenderSwitch.isOn
+        
+       // UIImage.pngData(profileImageView.image)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            profileImageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
